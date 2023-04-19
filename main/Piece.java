ab6c88a0e3079f1dfcf72ea5579c771efb7dbe3c
Marcus <mherbert@unbc.ca>
2023-04-19 15:26:22 -0700

    Sketching out Piece interface

--- main/Piece.java
+++ main/Piece.java
@@ -1,5 +1,14 @@
 package main;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 public interface Piece {
+
+    int value();
+    Color getColor();
+    ArrayList<Move> possibleMoves();
+    ImageIcon getImage();
 
 }
